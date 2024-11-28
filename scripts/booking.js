447584cf5d7807bd1ea1d9f9755ed5c959103dae
Pratham Patel <prathamp0422@gmail.com>
2024-11-27 21:07:36 -0700

    upload

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,49 +3,49 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
-let costPerDay = document.getElementById('calculated-cost')
-let numDaySelected = 0
-let dailyRate = 35
+let costPerDay = document.getElementById('calculated-cost');
+let numDaySelected = 0;
+let dailyRate = 35;
 
 
-let fulldayLenght = document.getElementById('full')
-let halfdayLenght = document.getElementById('half')
+let fulldayLenght = document.getElementById('full');
+let halfdayLenght = document.getElementById('half');
 
 
-let monday= document.getElementById('monday')
-let tuesday= document.getElementById('tuesday')
-let wednesday= document.getElementById('wednesday')
-let thursday= document.getElementById('thursday')
-let friday= document.getElementById('friday')
+let monday= document.getElementById('monday');
+let tuesday= document.getElementById('tuesday');
+let wednesday= document.getElementById('wednesday');
+let thursday= document.getElementById('thursday');
+let friday= document.getElementById('friday');
 
 
-let clearBtn = document.getElementById('clear-button')
+let clearBtn = document.getElementById('clear-button');
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
 monday.addEventListener("click", function(){
-    colorChangeDay(monday)
+    colorChangeDay(monday);
 });
 tuesday.addEventListener("click", function(){
-    colorChangeDay(tuesday)
+    colorChangeDay(tuesday);
 });
 wednesday.addEventListener("click", function(){
-    colorChangeDay(wednesday)
+    colorChangeDay(wednesday);
 });
 thursday.addEventListener("click", function(){
-    colorChangeDay(thursday)
+    colorChangeDay(thursday);
 });
 friday.addEventListener("click", function(){
-    colorChangeDay(friday)
+    colorChangeDay(friday);
 });
 
 function colorChangeDay(day){
     if(day.classList.contains("clicked") == false){
         day.classList.add("clicked");
-        numDaySelected = numDaySelected + 1
-        reCalculationCost(dailyRate, numDaySelected)
+        numDaySelected = numDaySelected + 1;
+        reCalculationCost(dailyRate, numDaySelected);
         }
     return;
 }
@@ -53,7 +53,7 @@ function colorChangeDay(day){
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
-clearBtn.addEventListener("click", clearDays)
+clearBtn.addEventListener("click", clearDays);
 
 function clearDays(){
     monday.classList.remove("clicked");
@@ -69,27 +69,27 @@ function clearDays(){
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-halfdayLenght.addEventListener("click", halfDayChange_rate)
+halfdayLenght.addEventListener("click", halfDayChange_rate);
 
 function halfDayChange_rate(){
     if(fulldayLenght.classList.contains("clicked") == true){
         fulldayLenght.classList.remove("clicked");
         halfdayLenght.classList.add("clicked");
-        dailyRate = 20
-        reCalculationCost(dailyRate,numDaySelected)
+        dailyRate = 20;
+        reCalculationCost(dailyRate,numDaySelected);
     }
 }
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
-fulldayLenght.addEventListener("click", fullDayChange_rate)
+fulldayLenght.addEventListener("click", fullDayChange_rate);
 
 function fullDayChange_rate(){
     if(halfdayLenght.classList.contains("clicked") == true){
         halfdayLenght.classList.remove("clicked");
         fulldayLenght.classList.add("clicked");
-        dailyRate = 35
-        reCalculationCost(dailyRate,numDaySelected)
+        dailyRate = 35;
+        reCalculationCost(dailyRate,numDaySelected);
     }
 }
 
@@ -100,5 +100,5 @@ function fullDayChange_rate(){
 
 
 function reCalculationCost (dailyRate, numDaySelected){
-    costPerDay.innerHTML = dailyRate * numDaySelected
+    costPerDay.innerHTML = dailyRate * numDaySelected;
 }
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,9 +1,9 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
-let contactPage = document.getElementById('contact-page')
+let contactPage = document.getElementById('contact-page');
 let form = document.querySelector('form'); 
-let h2 = document.querySelector('h2')
-let newPara = document.createElement('p')
+let h2 = document.querySelector('h2');
+let newPara = document.createElement('p');
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
 form.addEventListener('submit', function(event) { 
